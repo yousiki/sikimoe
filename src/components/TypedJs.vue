7dc878ddc7d09e16d6b1139cf11d1eab1d7a8d66
YouSiki <you.siki@outlook.com>
2025-04-24 01:41:31 +0800

    Refactor code structure for improved readability and maintainability

--- src/components/TypedJs.vue
+++ src/components/TypedJs.vue
@@ -5,7 +5,24 @@ import { onMounted, ref } from 'vue';
 
 const strings = ref<Array<string>>([]);
 
+// Function to randomly select a welcome cover image
+const selectRandomWelcomeCover = () => {
+  // Number of welcome cover images available (update this as you add more images)
+  const numImages = 9; // We have 9 images (0-8)
+  const randomIndex = Math.floor(Math.random() * numImages);
+  const imagePath = `/images/welcome-covers-resized/welcome-cover-${randomIndex}.jpg`;
+
+  // Set the CSS variable for the background image
+  document.documentElement.style.setProperty(
+    '--welcome-cover-image',
+    `url('${imagePath}')`
+  );
+};
+
 onMounted(() => {
+  // Select a random image when the component loads
+  selectRandomWelcomeCover();
+
   axios.get('https://v1.hitokoto.cn/').then((res: any) => {
     const hitokotoRes = JSON.parse(JSON.stringify(res.data));
     strings.value = [hitokotoRes.hitokoto, hitokotoRes.from];
@@ -45,7 +62,10 @@ onMounted(() => {
   width: 100%;
   height: 100%;
   position: absolute;
-  background-image: url('/welcome-cover.jpg');
+  background-image: var(
+    --welcome-cover-image,
+    url('/images/welcome-covers-resized/welcome-cover-0.jpg')
+  );
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
